b7ef5eb2b7cbaebc6a9d4f2f3214d123a668ecc5
ekoapriliyani <72810997+ekoapriliyani@users.noreply.github.com>
2025-07-10 15:00:44 +0700

    list

--- list.py
+++ list.py
@@ -1,2 +1,13 @@
 thislist = ["apple", "banana", "cherry"]
 print(thislist)
+
+
+#list length
+thislist = ["apple", "banana", "cherry"]
+print(len(thislist))
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:5])
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[:4])
