887e69c7a75778c3a7c8f4ff924648c8444df964
ekoapriliyani <72810997+ekoapriliyani@users.noreply.github.com>
2025-07-14 13:37:52 +0700

    list

--- list.py
+++ list.py
@@ -11,3 +11,6 @@ print(thislist[2:5])
 
 thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
 print(thislist[:4])
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[-4:-1])
